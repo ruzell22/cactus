841bb39d183f46c5026ab7babc0ec38927dc4bf4
github-action-benchmark <github@users.noreply.github.com>
2024-01-15 00:58:31 +0000

    add Benchmark (benchmarkjs) benchmark result for 0ce33940cc5367d0960e43dd8116cffbe656ca4c

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1705280310955,
+  "repoUrl": "https://github.com/ruzell22/cactus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "ruzell22",
+            "username": "ruzell22"
+          },
+          "committer": {
+            "name": "ruzell22",
+            "username": "ruzell22"
+          },
+          "id": "0ce33940cc5367d0960e43dd8116cffbe656ca4c",
+          "message": "Jmeterbenchmark",
+          "timestamp": "2022-01-17T07:08:58Z",
+          "url": "https://github.com/ruzell22/cactus/pull/1/commits/0ce33940cc5367d0960e43dd8116cffbe656ca4c"
+        },
+        "date": 1705280309465,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1619456,
+            "range": "±0.20%",
+            "unit": "ops/sec",
+            "extra": "95 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 13074,
+            "range": "±1.75%",
+            "unit": "ops/sec",
+            "extra": "93 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
