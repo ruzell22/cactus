e4fd0270219cdfcab8e766ee7b0b242b53f70659
github-action-benchmark <github@users.noreply.github.com>
2024-05-13 01:14:17 +0000

    add Benchmark (benchmarkjs) benchmark result for d9577b89b6a6cd4ae60318beb60b1f278e3ab53c

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1715562857420,
+  "repoUrl": "https://github.com/ruzell22/cactus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "d9577b89b6a6cd4ae60318beb60b1f278e3ab53c",
+          "message": "build: bump web3-* to v4.2.1 in plugin-htlc-coordinator-besu\n\nBumps [web3-utils](https://github.com/ChainSafe/web3.js) from 4.0.3 to 4.2.1.\n- [Release notes](https://github.com/ChainSafe/web3.js/releases)\n- [Changelog](https://github.com/web3/web3.js/blob/4.x/CHANGELOG.md)\n- [Commits](https://github.com/ChainSafe/web3.js/compare/v4.0.3...v4.2.1)\n\n---\nupdated-dependencies:\n- dependency-name: web3-utils\n  dependency-type: direct:development\n...\n\nCo-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>\n\nSigned-off-by: dependabot[bot] <support@github.com>\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-05-12T00:04:06-07:00",
+          "tree_id": "e49f65a4e1bf3d44eca27356586929ec920c110e",
+          "url": "https://github.com/ruzell22/cactus/commit/d9577b89b6a6cd4ae60318beb60b1f278e3ab53c"
+        },
+        "date": 1715562853460,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 758,
+            "range": "±2.62%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
