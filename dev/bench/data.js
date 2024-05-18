832e09524e1616814d8c206c52f8660592a4796d
github-action-benchmark <github@users.noreply.github.com>
2024-05-18 17:37:01 +0000

    add Benchmark (benchmarkjs) benchmark result for caa2b3a50ab7928942e72341ebc7e5511e3054f7

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716053820799,
+  "repoUrl": "https://github.com/ruzell22/cactus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sandeep.nishad1@ibm.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "caa2b3a50ab7928942e72341ebc7e5511e3054f7",
+          "message": "fix(corda-simple-app): use correct bond asset flows and contracts for bond asset exchange\n\n    corda-simple-app: fix typo for partyb twice in initNetworkId.sh\n\nSigned-off-by: Sandeep Nishad <sandeep.nishad1@ibm.com>",
+          "timestamp": "2024-05-16T21:25:53-07:00",
+          "tree_id": "93b26c979a99de08a324dc8c2802d4002c0bd719",
+          "url": "https://github.com/ruzell22/cactus/commit/caa2b3a50ab7928942e72341ebc7e5511e3054f7"
+        },
+        "date": 1716053818417,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 579,
+            "range": "±1.63%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 325,
+            "range": "±17.87%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
