dabed796bc9e62435a22504a186018b5502f25de
github-action-benchmark <github@users.noreply.github.com>
2024-05-21 04:02:17 +0000

    add Benchmark (benchmarkjs) benchmark result for eacaef3cc52512b74b11cdc1b8c1b09e4d82460e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716263104749,
+  "lastUpdate": 1716264137258,
   "repoUrl": "https://github.com/ruzell22/cactus",
   "entries": {
     "Benchmark": [
@@ -105,6 +105,37 @@ window.BENCHMARK_DATA = {
             "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
             "value": 365,
             "range": "±1.24%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "eacaef3cc52512b74b11cdc1b8c1b09e4d82460e",
+          "message": "chore(github): shorten ci.yaml workflow and job names\n\nce - cactus-example\ncp - cactus-plugin\ncpk - cactus-plugin-keychain\ncpl - cactus-plugin-ledger\ncplc - cactus-plugin-ledger-connector\nplc - plugin-ledger-connector\ncpp - cactus-plugin-persistence\nct - cactus-test\nctp - cactus-test-plugin\n\nFixes: hyperledger#2624\n\nSigned-off-by: zondervancalvez <zondervan.v.calvez@accenture.com>",
+          "timestamp": "2024-05-18T16:17:18-07:00",
+          "tree_id": "36312b30100d2d6e1069e72c17470d211eadc167",
+          "url": "https://github.com/ruzell22/cactus/commit/eacaef3cc52512b74b11cdc1b8c1b09e4d82460e"
+        },
+        "date": 1716264134663,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 771,
+            "range": "±2.71%",
             "unit": "ops/sec",
             "extra": "181 samples"
           }
