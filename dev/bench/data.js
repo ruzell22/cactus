c2e9fbd06fc59108a794f74ebd1da3d87df88385
github-action-benchmark <github@users.noreply.github.com>
2024-08-06 03:12:25 +0000

    add Benchmark (benchmarkjs) benchmark result for fdce6b3222fbec1c0f05db41dd5b93fbc8a8939d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722306409206,
+  "lastUpdate": 1722913944937,
   "repoUrl": "https://github.com/ruzell22/cactus",
   "entries": {
     "Benchmark": [
@@ -290,6 +290,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.85%",
             "unit": "ops/sec",
             "extra": "178 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aldousss.alvarez@gmail.com",
+            "name": "aldousalvarez",
+            "username": "aldousalvarez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "fdce6b3222fbec1c0f05db41dd5b93fbc8a8939d",
+          "message": "ci(connector-xdai): fix docker rate limit issues with openethereum image pull\n\nPrimary Changes\n----------------\n1. Migrated all the xdai connector tests to besu ledger\n   images that is being pulled from ghcr\n\nFixes #3413\n\nSigned-off-by: aldousalvarez <aldousss.alvarez@gmail.com>",
+          "timestamp": "2024-08-01T10:57:28-07:00",
+          "tree_id": "2fecc0a53a69a02fe88066a2239c35ccea8728a9",
+          "url": "https://github.com/ruzell22/cactus/commit/fdce6b3222fbec1c0f05db41dd5b93fbc8a8939d"
+        },
+        "date": 1722913942550,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 602,
+            "range": "±1.70%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 363,
+            "range": "±1.36%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
           }
         ]
       }
